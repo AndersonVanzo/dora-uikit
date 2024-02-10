d92acab6a18763f199490c313755b83134fd09ff
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-10 16:29:42 -0300

    feat(icon): add Icon component

---
+++ src/icons/icon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type { IconType } from '../types/icon';
+import type { ColorValue } from 'react-native';
+import { getIconFilePath } from './utils/getIconFilePath';
+import { SvgUri } from 'react-native-svg';
+
+export type IconProps = {
+  color: ColorValue;
+  size: number;
+  type: IconType;
+};
+
+export const Icon = ({ color, size, type }: IconProps) => {
+  const iconPath = getIconFilePath(type);
+
+  if (!iconPath) {
+    return null;
+  }
+
+  return <SvgUri color={color} height={size} uri={iconPath} width={size} />;
+};
