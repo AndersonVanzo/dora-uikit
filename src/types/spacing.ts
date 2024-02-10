f2e6ec387364bf9a9a3c16ec47d1a4c6c572be26
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-10 14:51:04 -0300

    feat(types): add SpacingVariants type

---
+++ src/types/spacing.ts
@@ -0,0 +1,10 @@
+export type SpacingVariants = {
+  None: number;
+  ExtraSmall: number;
+  Smaller: number;
+  Small: number;
+  Medium: number;
+  Large: number;
+  Larger: number;
+  ExtraLarge: number;
+};
