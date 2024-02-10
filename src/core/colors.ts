26d0a7c848583f8f96035c50b2518869f2c05392
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-10 14:08:23 -0300

    feat(core): add Colors config

---
+++ src/core/colors.ts
@@ -0,0 +1,37 @@
+export const Colors = {
+  Black: '#27272B',
+  White: '#FAFAFA',
+
+  LightestGray: '#F5F5F5',
+  MediumGray: '#EFEEEE',
+  ThirdGray: '#D9D9D9',
+
+  ThirdGreen: '#B3F6DA',
+  MediumGreen: '#42E8A2',
+  Green: '#4CD49B',
+
+  ThirdRed: '#F6B3B3',
+  MediumRed: '#E84141',
+  Red: '#D12222',
+
+  ThirdYellow: '#FCE7AA',
+  MediumYellow: '#F7C42A',
+  Yellow: '#E5B72A',
+
+  ThirdOrange: '#FAD6B6',
+  MediumOrange: '#E9AE67',
+  Orange: '#F39948',
+
+  ThatBlue: '#113DBF',
+  MediumBlue: '#3F6CF2',
+  ThirdBlue: '#48A1F3',
+  LightestBlue: '#B3CBF6',
+
+  Teal: '#2CC5CD',
+  MediumTeal: '#35CED6',
+  ThirdTeal: '#AEEBEF',
+
+  Purple: '#52179B',
+  MediumPurple: '#8331E8',
+  ThirdPurple: '#CDADF6',
+};
