1a62498a1cf23014422616836a1cf98bf6f971c0
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-10 14:29:02 -0300

    feat(types): add ColorsVariants type

---
+++ src/types/colors.ts
@@ -0,0 +1,39 @@
+import type { ColorValue } from 'react-native';
+
+export type ColorsVariants = {
+  Black: ColorValue;
+  White: ColorValue;
+
+  LightestGray: ColorValue;
+  MediumGray: ColorValue;
+  ThirdGray: ColorValue;
+
+  ThirdGreen: ColorValue;
+  MediumGreen: ColorValue;
+  Green: ColorValue;
+
+  ThirdRed: ColorValue;
+  MediumRed: ColorValue;
+  Red: ColorValue;
+
+  ThirdYellow: ColorValue;
+  MediumYellow: ColorValue;
+  Yellow: ColorValue;
+
+  ThirdOrange: ColorValue;
+  MediumOrange: ColorValue;
+  Orange: ColorValue;
+
+  ThatBlue: ColorValue;
+  MediumBlue: ColorValue;
+  ThirdBlue: ColorValue;
+  LightestBlue: ColorValue;
+
+  Teal: ColorValue;
+  MediumTeal: ColorValue;
+  ThirdTeal: ColorValue;
+
+  Purple: ColorValue;
+  MediumPurple: ColorValue;
+  ThirdPurple: ColorValue;
+};
