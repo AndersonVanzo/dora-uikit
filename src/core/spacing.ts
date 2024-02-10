b0a589ac198f364712431c9a6e48866573e15daa
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-02-10 14:52:44 -0300

    feat(core): add Spacing configs

---
+++ src/core/spacing.ts
@@ -0,0 +1,12 @@
+import type { SpacingVariants } from '../types/spacing';
+
+export const Spacing: SpacingVariants = {
+  None: 0,
+  ExtraSmall: 2,
+  Smaller: 4,
+  Small: 8,
+  Medium: 16,
+  Large: 24,
+  Larger: 32,
+  ExtraLarge: 40,
+};
